c851f582a81c0b3f969c995d212353e91d5ef936
francoperino <fperino@frsf.utn.edu.ar>
2020-10-05 01:10:34 -0300

    encontre lo de gradiente en 8 direcciones

--- main.m
+++ main.m
@@ -8,7 +8,7 @@ test_ds = imageDatastore('training_data\test', 'IncludeSubfolders',true,'LabelSo
 % Training files for each emotion, limited by the minimum number for a
 % given label
 numTrainFiles = 350;
-numTestFiles = 100;
+numTestFiles = 50;
 [imgs_ds_train] = splitEachLabel(train_ds,numTrainFiles,'randomize');
 [imgs_ds_test] = splitEachLabel(test_ds,numTestFiles,'randomize');
  
